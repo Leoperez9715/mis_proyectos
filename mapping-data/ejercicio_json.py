0d231ca46ccc2fbafa40942025beffa2f6e9ec0d
EDWAR PEREZ <leoperez9715@gmail.com>
2025-02-18 19:49:59 -0500

    Se agregaron comentarios

---
+++ mapping-data/ejercicio_json.py
@@ -0,0 +1,44 @@
+from csv import DictReader
+import pandas as pd
+
+#USO DE CSV
+with open('sample_data/wine-ratings-small.csv', encoding='utf-8', errors='ignore') as f:
+    reader = DictReader(f)
+    wines = list(reader)
+    #lista de la forma [{k:v},{k:v}]
+    #Es una lista de diccionarios
+#print(type(reader))
+#print(type(wines),len(wines))
+
+#USO DE PANDAS 
+df = pd.read_csv('sample_data/wine-ratings-small.csv')
+#print(df["variety"])
+    #** La información se importo correctamente
+
+print(df.columns)
+    #** Se obtiene los nombres de las columnas para trabajar en ellas
+
+#Proceso de filtrado
+print("las dimensiones de la tabla son: ", df.size)
+
+
+#Filter = (df.loc[df["variety"] == "White Wine"]) & (df["rating"]>=90) &(df["rating"]<=92)
+Filter = (df["rating"]>=90) & (df['rating']<= 92) & (df["variety"] == "Red Wine")
+    # Filtro N°1: la calificación del vino debe estar entre 90 y 92 
+    # Filtro N°2: el tipo de vino debe ser Red wine 
+
+#Con el filtro, ahora se aplica al df para obtener el nuevo dataframe 
+new_df = df[Filter]
+
+
+# Se revisa el data frame para ver si funciono la implementación 
+print(Filter)
+print(new_df.iloc[:20,0:6])
+print("las dimensiones de la nueva tabla son: ", new_df.size)
+
+
+
+#USO COMO DICCIONARIO 
+#Enviar la tabla a diccionario
+dict_data = df.to_dict()
+#print(dict_data["variety"][0],dict_data["rating"][0],dict_data["region"][0])
